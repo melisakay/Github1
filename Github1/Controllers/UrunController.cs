935e234c913bb5350d6d828226836c60c980f548
Melis Akay <melisbegumakay@yahoo.com>
2023-07-11 20:33:56 +0300

    Urun controllerı eklendi

---
+++ Github1/Controllers/UrunController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Github1.Controllers
+{
+    public class UrunController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
